15d131a05e5b17644a8ab58073721eea7e8a9171
saif-elhady <saifelhady2004@gmail.com>
2024-08-03 23:55:54 +0300

    Add a document interface

--- src/models/schema.ts
+++ src/models/schema.ts
@@ -1,5 +1,13 @@
 import mongoose, { Schema } from "mongoose";
 
+export interface ITrip extends Document {
+    departurePlace: string;
+    destination: string;
+    startingDate: Date;
+    duration: String;
+    passengersNumber: number;
+}
+
 const tripSchema = new Schema({
     departurePlace: {
         type: String,
@@ -10,7 +18,7 @@ const tripSchema = new Schema({
         required: true
     },
     startingDate: {
-        type: Date,
+        type: String,
         required: true
     },
     duration: {
@@ -19,8 +27,26 @@ const tripSchema = new Schema({
     },
     passengersNumber: {
         type: Number,
-        required: true
+        required: true,
+        min:[2, 'The number of passengers must be greater than 1']
     }
-})
+})  
 
+// //change Date format to  
+// function formatDate(date: Date): string {
+//     const day = String(date.getDate()).padStart(2, '0');
+//     const month = String(date.getMonth() + 1).padStart(2, '0'); // Months are zero-based
+//     const year = date.getFullYear();
+//     return `${day}/${month}/${year}`;
+// }
+
+// tripSchema.pre('save', function (next) {
+//     const trip = this;
+//     if (trip.isModified('startingDate')) {
+//         const date = new Date(trip.startingDate);
+//         const formattedDate = formatDate(date);
+//         trip.startingDate = formattedDate;
+//     }
+//     next();
+// });
 export const Trip = mongoose.model('Trip', tripSchema);
