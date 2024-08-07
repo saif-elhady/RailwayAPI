d09cee45252a3924b0036863d311810f9654fffb
saif-elhady <saifelhady2004@gmail.com>
2024-08-07 15:25:05 +0300

    fix: Add validators for the query parameters

--- dist/routes/router.js
+++ dist/routes/router.js
@@ -3,8 +3,22 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = require("express");
 const schema_1 = require("../models/schema");
 const apiFeatures_1 = require("../utils/apiFeatures");
+const express_validator_1 = require("express-validator");
 const router = (0, express_1.Router)();
-router.get('/trips', async (req, res) => {
+const isValidDate = (value) => {
+    const regex = /^(0[1-9]|[12][0-9]|3[01])\/(0[1-9]|1[0-2])\/\d{4}$/;
+    if (!regex.test(value)) {
+        throw new Error('Date must be in dd/mm/yyyy format');
+    }
+    return true;
+};
+router.get('/trips', [
+    (0, express_validator_1.check)('page').optional().isInt({ min: 1 }).withMessage('Page must be a positive integer'),
+    (0, express_validator_1.check)('limit').optional().isInt({ min: 1 }).withMessage('limit must be a positive integer'),
+    (0, express_validator_1.check)('sort').optional().isString().withMessage('sort must be a string'),
+    (0, express_validator_1.check)('date_from').optional().custom(isValidDate).withMessage('Date from must be in dd/mm/yyyy format'),
+    (0, express_validator_1.check)('date_till').optional().custom(isValidDate).withMessage('Date till must be in dd/mm/yyyy format')
+], async (req, res) => {
     const features = new apiFeatures_1.APIFeatures(schema_1.Trip.find(), req.query)
         .paginate()
         .sort()
--- src/routes/router.ts
+++ src/routes/router.ts
@@ -1,9 +1,24 @@
 import express, { Router } from "express";
 import { Trip } from '../models/schema';
 import { APIFeatures } from "../utils/apiFeatures";
+import { check, validationResult } from "express-validator";
 const router = Router();
 
-router.get('/trips', async (req, res) => {
+const isValidDate = (value: string) => {
+    const regex = /^(0[1-9]|[12][0-9]|3[01])\/(0[1-9]|1[0-2])\/\d{4}$/;
+    if (!regex.test(value)) {
+        throw new Error('Date must be in dd/mm/yyyy format');
+    }
+    return true;
+};
+
+router.get('/trips', [
+    check('page').optional().isInt({ min: 1 }).withMessage('Page must be a positive integer'),
+    check('limit').optional().isInt({ min: 1 }).withMessage('limit must be a positive integer'),
+    check('sort').optional().isString().withMessage('sort must be a string'),
+    check('date_from').optional().custom(isValidDate).withMessage('Date from must be in dd/mm/yyyy format'),
+    check('date_till').optional().custom(isValidDate).withMessage('Date till must be in dd/mm/yyyy format')
+],async (req: express.Request, res: express.Response) => {
     const features = new APIFeatures(Trip.find(), req.query)
         .paginate()
         .sort()
